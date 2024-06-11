2d2eb662ed242f4b88e4249c297108754b9555b5
Evelyn França <evelynsfrancaa@gmail.com>
2024-06-10 21:59:46 -0300

    chore: added about section

--- app/home/features/about/index.tsx
+++ app/home/features/about/index.tsx
@@ -1,8 +1,30 @@
+import Link from "next/link";
 import styles from "./index.module.css";
+import Image from "next/image";
+import aboutImage from '@/public/02-office.jpg';
 
-export default function AboutSection() {    
+export default function AboutSection() {
     return (
-        <section id="about" className={styles.section}>About</section>
+        <section id="about" className={styles.section}>            
+
+            <article className={styles.article}>
+                <Image src={aboutImage} alt="about-image" />
+            </article>
+
+            <article className={styles.article}>
+
+                <header className={styles.header}>
+                    <h2>Sobre mim</h2>
+                </header>
+
+                <div className={styles.content}>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error aut reiciendis amet cupiditate eum ipsa doloribus, sequi, quasi laboriosam soluta facilis natus voluptatem hic quo numquam blanditiis iure eius reprehenderit.</p>
+                </div>
+
+                <Link href="/about" className={styles.button}>Saiba mais</Link>
+                
+            </article>
+
+        </section>
     );
-  }
-  
+}
