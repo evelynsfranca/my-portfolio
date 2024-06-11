396b687d5b34cc7f92ad936d88ddcdf76de33a70
Evelyn França <evelynsfrancaa@gmail.com>
2024-06-11 15:07:59 -0300

    fix: components menu and footer css fixes

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -17,51 +17,53 @@ export default function Footer() {
 
   return (
     <footer className={styles.footer}>
-      
+
       <article className={styles.text}>
         <div>Desenvolvido por Evelyn França</div>
         <FontAwesomeIcon icon={faHeart} />
       </article>
 
-      {currentPage != "/" && (
-        <article className={styles.socialMedia}>
-          <Link
-            href="https://www.instagram.com/edsf_per"
-            target="_blank"
-            className={styles.socialMediaLink}
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/evelynsfranca/"
-            target="_blank"
-            className={styles.socialMediaLink}
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </Link>
-          <Link
-            href="https://github.com/evelynsfranca"
-            target="_blank"
-            className={styles.socialMediaLink}
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </Link>
-          <Link
-            href="https://wa.me/5541984017050/?&text&type=phone_number&app_absent=1"
-            target="_blank"
-            className={styles.socialMediaLink}
-          >
-            <FontAwesomeIcon icon={faWhatsapp} />
-          </Link>
-          <Link
-            href="mailto:francasevelyn@gmail.com"
-            target="_blank"
-            className={styles.socialMediaLink}
-          >
-            <FontAwesomeIcon icon={faEnvelope} />
-          </Link>
-        </article>
-      )}
+      {currentPage != "/"
+        && currentPage != "/contact"
+        && (
+          <article className={styles.socialMedia}>
+            <Link
+              href="https://www.instagram.com/edsf_per"
+              target="_blank"
+              className={styles.socialMediaLink}
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/evelynsfranca/"
+              target="_blank"
+              className={styles.socialMediaLink}
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </Link>
+            <Link
+              href="https://github.com/evelynsfranca"
+              target="_blank"
+              className={styles.socialMediaLink}
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </Link>
+            <Link
+              href="https://wa.me/5541984017050/?&text&type=phone_number&app_absent=1"
+              target="_blank"
+              className={styles.socialMediaLink}
+            >
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </Link>
+            <Link
+              href="mailto:francasevelyn@gmail.com"
+              target="_blank"
+              className={styles.socialMediaLink}
+            >
+              <FontAwesomeIcon icon={faEnvelope} />
+            </Link>
+          </article>
+        )}
     </footer>
   );
 }
