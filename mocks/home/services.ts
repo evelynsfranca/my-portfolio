762750abc74dbdd066a57dfd46d1ff78b5dbb189
Evelyn <evelynsfrancaa@gmail.com>
2024-08-24 19:32:27 -0300

    fix: home page sections headers standardzation

--- mocks/home/services.ts
+++ mocks/home/services.ts
@@ -3,24 +3,28 @@ import { faChartSimple, faComputer, faDesktop, faTerminal } from "@fortawesome/f
 
 export const services: ServiceProps[] = [
     {
+        id: 1,
         title: "Service 1",
         description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi aut nisi velit rem asperiores est labore laboriosam cupiditate? Quam repudiandae quibusdam dicta magni odio consequatur error beatae dolores non similique.",
         icon: faComputer,
     },
     {
+        id: 2,
         title: "Service 2",
         description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi aut nisi velit rem asperiores est labore laboriosam cupiditate? Quam repudiandae quibusdam dicta magni odio consequatur error beatae dolores non similique.",
         icon: faDesktop,
     },
     {
+        id: 3,
         title: "Service 3",
         description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi aut nisi velit rem asperiores est labore laboriosam cupiditate? Quam repudiandae quibusdam dicta magni odio consequatur error beatae dolores non similique.",
         icon: faTerminal,
     },
     {
+        id: 4,
         title: "Service 4",
         description:
             "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi aut nisi velit rem asperiores est labore laboriosam cupiditate? Quam repudiandae quibusdam dicta magni odio consequatur error beatae dolores non similique.",
