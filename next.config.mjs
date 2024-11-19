674f0ded82ee4163dee91960fc46d1d374b5b4b1
Evelyn <evelynsfrancaa@gmail.com>
2024-11-19 15:09:43 -0300

    chore: added next.config environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        API_URL: process.env.API_URL,
+        RESEND_TOKEN: process.env.RESEND_TOKEN,
+        RESEND_MAIL_FROM: process.env.RESEND_MAIL_FROM,
+        RESEND_MAIL_TO: process.env.RESEND_MAIL_TO
+    },
+};
 
 export default nextConfig;
