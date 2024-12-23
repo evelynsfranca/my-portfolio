e22b84d7b4b11d9a0ee9895330a712ab04373320
Evelyn França <evelynsfrancaa@gmail.com>
2024-12-23 16:17:57 -0300

    fix: services card home feature refactor

--- app/home/features/services/index.tsx
+++ app/home/features/services/index.tsx
@@ -8,7 +8,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useRef, useState } from "react";
-import ServiceCard from "./components";
+import ServiceCard from "./components/card";
 import styles from "./index.module.css";
 
 interface ButtonStyle {
