471451be2d029f20a3a559174b12de1d52bdc0ce
Evelyn França <evelynsfrancaa@gmail.com>
2024-12-23 15:37:47 -0300

    chore: services page refactor

---
+++ app/services/components/card/page.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import ButtonLink from "@/components/Button/Link";
+import { ServiceModel } from "@/models/ServiceModel";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import styles from "./page.module.css";
+
+export interface ServiceCardProps {
+  index: number;
+  service: ServiceModel;
+}
+
+export default function ServiceCard(props: ServiceCardProps) {
+
+  const { index, service } = props;
+
+  const cardRef = useRef<HTMLElement>(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    if (cardRef.current
+      && (cardRef.current?.getBoundingClientRect().top <= window.innerHeight - 150
+        || cardRef.current?.getBoundingClientRect().bottom <= window.innerHeight)) {
+      cardRef.current.classList.add(styles.active);
+    }
+  }, [scrollPosition]);
+
+  useEffect(() => {
+    const handleScroll = () => setScrollPosition(document.body.scrollTop);
+    document.body.addEventListener('scroll', handleScroll);
+    return () => document.body.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <article
+      className={styles.card}
+      id={service.id.toString()}
+      ref={cardRef}
+    >
+      <article className={styles.description}>
+        <div>
+          <article>
+            <header className={styles.header}>
+              <Image
+                className={styles.image}
+                src={service.image}
+                alt={service?.title}
+                height={0}
+                width={0}
+                sizes="100vw"
+              />
+
+              <h2 className={styles.title}>
+                {service?.title}
+              </h2>
+            </header>
+
+            <div>
+              <p dangerouslySetInnerHTML={{ __html: service?.description }} />
+
+              <ButtonLink
+                color={index % 2 > 0 ? "primary" : "secondary"}
+                label="Solicitar orçamento"
+                type="button"
+                url={`/contact?service=${service.id}`}
+                target="_self"
+              />
+            </div>
+          </article>
+        </div>
+      </article>
+    </article>
+  );
+}
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,99 +1,10 @@
 'use client';
 
 import { services } from "@/data/services/services";
-import { faX } from "@fortawesome/free-solid-svg-icons";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons/faChevronLeft";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "next/image";
-import { useParams, useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import ServiceCard from "./components/card/page";
 import styles from "./page.module.css";
-import { ServiceModel } from "@/models/ServiceModel";
-
-export type ServiceAnimation = {
-  transform: string;
-  opacity: number;
-}
-
-export type MobileAnimation = {
-  animation: string;
-}
 
 export default function Services() {
-
-  const router = useRouter();
-  const params = useParams();
-
-  const navElementRef = useRef<HTMLElement>(null);
-  const [active, setActive] = useState<number>();
-  const [open, setOpen] = useState<boolean>(false);
-
-  const handleNavScroll = (direction?: number) => {
-
-    let scrollSize = 0;
-    let scroll = 0;
-
-    if (navElementRef.current && active != undefined) {
-      scrollSize = navElementRef.current.getBoundingClientRect().width;
-      scroll = direction
-        ? direction * scrollSize
-        : active > 0
-          ? (active - 1) * scrollSize
-          : 0;
-
-      navElementRef.current.scroll({ left: scroll, behavior: 'smooth' });
-    }
-  }
-
-  const handleService = (key: number, type: 'button' | 'item') => {
-    setOpen(true);
-
-    let newActive;
-
-    if (type === 'item') {
-
-      newActive = key;
-      (open && active == key) && setOpen(false);
-
-    } else {
-      if (key == 1 && active != undefined && active < (services.length - 1)) // Botão da direita (Avançar)
-        newActive = active + 1;
-      else if (key == -1 && active != undefined && active > 0) // Botão da esquerda (Voltar)
-        newActive = active - 1;
-      else
-        newActive = 0;
-
-
-      handleNavScroll(key);
-    }
-
-    newActive != undefined && setActive(newActive);
-
-  }
-
-  const handleCloseDescription = () => {
-    setActive(undefined);
-    setOpen(false);
-    router.push('/services');
-  }
-
-  useEffect(() => {
-    handleNavScroll();
-
-    if (active != undefined && services[active] != undefined) {
-      router.push('/services#' + services[active].id);
-      setOpen(true);
-    }
-
-  }, [active]);
-
-  useEffect(() => {
-    let hash = window.location.hash.split('#')[1] || '';
-    let serviceIndex = services.indexOf(services.filter((item) => item.id === hash)[0]);
-
-    serviceIndex >= 0 && setActive(serviceIndex);
-  }, [params]);
 
   return (
     <main id="services" className={styles.main}>
@@ -112,89 +23,18 @@ export default function Services() {
 
         <div className={styles.content}>
 
-          <button
-            type="button"
-            className={`${styles.button} ${open && active != undefined && active > 0 && styles.active}`}
-            onClick={() => handleService(-1, 'button')}
-          >
-            <FontAwesomeIcon icon={faChevronLeft} />
-          </button>
-
-          <article
-            ref={navElementRef}
-            className={`${styles.items} ${open && styles.active}`}
-          >
+          <article className={styles.items}>
 
             {services.map((it, i) => (
-
-              <article
-                key={it.id}
-                className={styles.item}
-                id={it.id.toString()}
-              >
-
-                <header>
-                  <nav className={styles.nav}>
-                    <button
-                      key={it.id}
-                      type="button"
-                      className={`${styles.button} ${active === i && styles.active}`}
-                      onClick={() => handleService(i, 'item')}
-                    >
-                      <FontAwesomeIcon
-                        icon={it.icon}
-                        className={styles.icon}
-                      />
-                      <span>{it.title}</span>
-                    </button>
-                  </nav>
-                </header>
-
-                <article className={`${styles.description} ${active === i && styles.active}`}>
-                  <div>
-                    <header>
-                      <FontAwesomeIcon
-                        icon={faX}
-                        className={styles.closeIcon}
-                        onClick={handleCloseDescription}
-                      />
-                    </header>
-
-                    <article>
-                      <header className={styles.header}>
-                        <Image
-                          className={styles.image}
-                          src={it.image}
-                          alt={it?.title}
-                          height={0}
-                          width={0}
-                          sizes="100vw"
-                        />
-
-                        <h2 className={styles.title}>
-                          {it?.title}
-                        </h2>
-                      </header>
-
-                      <p dangerouslySetInnerHTML={{ __html: it?.description }} />
-                    </article>
-                  </div>
-                </article>
-
-              </article>
+              <ServiceCard 
+                key={it.id} 
+                index={i} 
+                service={it}
+              />
             ))}
+
           </article>
-
-          <button
-            type="button"
-            className={`${styles.button} ${open && active != undefined && active < (services.length - 1) && styles.active}`}
-            onClick={() => handleService(1, 'button')}
-          >
-            <FontAwesomeIcon icon={faChevronRight} />
-          </button>
-
         </div>
-
       </section>
     </main>
   );
