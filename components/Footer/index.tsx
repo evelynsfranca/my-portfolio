5b30dcc58e98f54da5ac2efa62f6ffebb00e0135
Evelyn França <evelynsfrancaa@gmail.com>
2024-12-29 18:02:04 -0300

    chore: Footer - refactor

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,39 +1,46 @@
 "use client";
 
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import styles from "./index.module.css";
+import { contacts } from "@/mocks/contacts";
+import { faHeart, faSquareFull } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { contacts } from "@/mocks/contacts";
+import styles from "./index.module.css";
 
 export default function Footer() {
+
   const currentPage = usePathname();
 
   return (
     <footer className={styles.footer}>
 
       <article className={styles.text}>
+
         <div>Desenvolvido por Evelyn França</div>
-        <FontAwesomeIcon icon={faHeart} />
+
+        <FontAwesomeIcon icon={faHeart} mask={faSquareFull} />
+
       </article>
 
       {currentPage != "/"
         && currentPage != "/contact"
         && (
           <article className={styles.socialMedia}>
-            {contacts.map(it => (
+
+            {contacts.map((it, i) => (
               <Link
-                key={it.link}
+                key={i}
                 href={it.link}
                 target="_blank"
                 className={styles.socialMediaLink}
               >
-                <FontAwesomeIcon icon={it.icon} />
+                <FontAwesomeIcon icon={it.icon} mask={faSquareFull} />
               </Link>
             ))}
+
           </article>
         )}
+
     </footer>
   );
 }
