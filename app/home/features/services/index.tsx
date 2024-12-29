bb2e8f5d15263eace6567dd55164f83f3c358710
Evelyn França <evelynsfrancaa@gmail.com>
2024-12-29 10:03:37 -0300

    chore: refactor CardSlider in home/features/services

--- app/home/features/services/index.tsx
+++ app/home/features/services/index.tsx
@@ -10,54 +10,16 @@ import styles from "./index.module.css";
 export default function ServicesSection() {
 
     const [slideItems, setSlideItems] = useState<number>(0);
-    const [cardHeight, setCardHeight] = useState<number>(0);
-    const [cardWidth, setCardWidth] = useState<number>(0);
-    const [documentSize, setDocumentSize] = useState<number>(0);
-    const [itemsHeight, setItemsHeight] = useState<number>(0);
-    const [itemsWidth, setItemsWidth] = useState<number>(0);
-
+    const [documentSize, setDocumentSize] = useState<number>();
 
     useEffect(() => {
+        let documentSize = document.body.offsetWidth;
 
-        let height = 0;
-        let width = 0;
-        let itemSize = document.getElementsByClassName("serviceCard");
-
-        Array.prototype.filter.call(
-            itemSize,
-            (it, _i) => it.getBoundingClientRect().height > height
-                ? height = it.getBoundingClientRect().height
-                : false
-        );
-        Array.prototype.filter.call(
-            itemSize,
-            (it, _i) => it.getBoundingClientRect().width > width
-                ? width = it.getBoundingClientRect().width
-                : false
-        );
-
-        setCardHeight(height);
-        setCardWidth(width);
-
-    }, [slideItems]);
-
-    useEffect(() => {
-        setItemsHeight(cardHeight * 2);
-
-        documentSize >= 670
-            ? setItemsWidth(cardWidth * 2)
-            : setItemsWidth(cardWidth);
-
-    }, [cardHeight, cardWidth]);
-
-    useEffect(() => {
         documentSize >= 670
             ? setSlideItems(4)
             : setSlideItems(2);
-    }, [documentSize]);
 
-    useEffect(() => {
-        setDocumentSize(document.body.offsetWidth);
+        setDocumentSize(documentSize);
     }, []);
 
     return (
@@ -68,15 +30,14 @@ export default function ServicesSection() {
             </header>
 
             <div className={styles.content}>
-
-                <CardSlider 
-                    itemsList={services} 
+                <CardSlider
+                    itemsList={services}
                     slideItemsQuantity={slideItems}
-                    itemsHeight={itemsHeight}
-                    itemsWidth={itemsWidth}
-                    gridRows={'1fr 1fr'}
-                    gridColumns={'1fr 1fr'}
+                    gridRows={2}
+                    gridColumns={documentSize && documentSize >= 640 ? 2 : 1}
                     gridFlow="column"
+                    totalItems={services.length}
+                    cardClass="serviceCard"
                 >
                     <>
                         {services.map((it, i) => (
@@ -91,7 +52,6 @@ export default function ServicesSection() {
                         ))}
                     </>
                 </CardSlider>
-
             </div>
 
             <footer>
