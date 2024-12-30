e2e090b9a170aefd54e5a6e30ec31ceeb820db1a
Evelyn França <evelynsfrancaa@gmail.com>
2024-12-30 16:05:01 -0300

    chore: Footer - refactor

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,25 +1,46 @@
 "use client";
 
-import { contacts } from "@/mocks/contacts";
+import { contacts } from "@/data/contacts";
 import { faHeart, faSquareFull } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 
 export default function Footer() {
 
   const currentPage = usePathname();
+  const [hoverStyle, setHoverStyle] = useState<number>();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
     <footer className={styles.footer}>
 
-      <article className={styles.text}>
+      <article 
+        className={styles.text} 
+        onMouseEnter={() => setHoverStyle(contacts.length)}
+        onMouseLeave={() => setHoverStyle(undefined)}
+      >
 
-        <div>Desenvolvido por Evelyn França</div>
+        <span>Desenvolvido por Evelyn França</span>
 
-        <FontAwesomeIcon icon={faHeart} mask={faSquareFull} />
-
+        {isClient && (
+          <FontAwesomeIcon
+            className={styles.icon}
+            icon={faHeart}
+            mask={faSquareFull}
+            style={{
+              background: hoverStyle == contacts.length
+                ? 'linear-gradient(45deg, var(--primary-color-variant), var(--secondary-color-variant))'
+                : 'white'
+            }}
+          />
+        )}
       </article>
 
       {currentPage != "/"
@@ -32,15 +53,27 @@ export default function Footer() {
                 key={i}
                 href={it.link}
                 target="_blank"
-                className={styles.socialMediaLink}
+                className={styles.link}
+                onMouseEnter={() => setHoverStyle(i)}
+                onMouseLeave={() => setHoverStyle(undefined)}
               >
-                <FontAwesomeIcon icon={it.icon} mask={faSquareFull} />
+                {isClient && (
+                  <FontAwesomeIcon
+                    className={styles.icon}
+                    icon={it.icon}
+                    mask={faSquareFull}
+                    style={{
+                      background: hoverStyle == i
+                        ? 'linear-gradient(45deg, var(--primary-color-variant), var(--secondary-color-variant))'
+                        : 'white'
+                    }}
+                  />
+                )}
               </Link>
             ))}
 
           </article>
         )}
-
     </footer>
   );
 }
