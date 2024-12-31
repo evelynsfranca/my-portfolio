737e9ad97d86562b549ae46946ce5e730403a81b
Evelyn França <evelynsfrancaa@gmail.com>
2024-12-31 19:26:03 -0300

    chore: Technologies - added list and logos

---
+++ data/technologies.ts
@@ -0,0 +1,220 @@
+import { TechnologyModel } from "@/models/TechnologyModel";
+import { faAws, faConfluence, faCss3, faDocker, faFigma, faGit, faGithub, faGitlab, faGulp, faHtml5, faJira, faJs, faNodeJs, faPhp, faReact, faSass, faVaadin } from "@fortawesome/free-brands-svg-icons";
+
+export const technologiesList: TechnologyModel[] = [
+    {
+        name: "JavaScript",
+        icon: faJs,
+        color: "#F0DB4F"
+    },
+    {
+        name: "Java",
+        icon: "/images/technologies/java.svg",
+        color: "#e32621"
+    },
+    {
+        name: "Git",
+        icon: faGit,
+        color: "#F1502F"
+    },
+    {
+        name: "GitHub",
+        icon: faGithub,
+        color: "#fff"
+    },
+    {
+        name: "Spring",
+        icon: "/images/technologies/spring.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "GitLab",
+        icon: faGitlab,
+        color: "#7f1f0d"
+    },
+    {
+        name: "HTML5",
+        icon: faHtml5,
+        color: "#e34c26"
+    },
+    {
+        name: "CSS3",
+        icon: faCss3,
+        color: "#264de4"
+    },
+    {
+        name: "ReactJS",
+        icon: faReact,
+        color: "#61DBFB"
+    },
+    {
+        name: "Azure",
+        icon: "/images/technologies/portal.svg",
+        color: "#61DBFB"
+    },
+    {
+        name: "Repos",
+        icon: "/images/technologies/repos.svg",
+        color: "#61DBFB"
+    },
+    {
+        name: "Logic Apps",
+        icon: "/images/technologies/logic-apps.svg",
+        color: "#61DBFB"
+    },
+    {
+        name: "Function Apps",
+        icon: "/images/technologies/function-apps.svg",
+        color: "#61DBFB"
+    },
+    {
+        name: "Storage Accounts",
+        icon: "/images/technologies/storage-accounts.svg",
+        color: "#61DBFB"
+    },
+    {
+        name: "Azure Tables",
+        icon: "/images/technologies/tables.svg",
+        color: "#61DBFB"
+    },
+    {
+        name: "App Registration",
+        icon: "/images/technologies/app-registrations.svg",
+        color: "#61DBFB"
+    },
+    {
+        name: "Terraform",
+        icon: "/images/technologies/terraform.svg",
+        color: "#61DBFB"
+    },
+    {
+        name: "Confluence",
+        icon: faConfluence,
+        color: "#35b0ff"
+    },
+    {
+        name: "Jira",
+        icon: faJira,
+        color: "#0d9aff"
+    },
+    {
+        name: "Docker",
+        icon: faDocker,
+        color: "#0d9aff"
+    },
+    {
+        name: "Figma",
+        icon: faFigma,
+        color: "#0d9aff"
+    },
+    {
+        name: "Python",
+        icon: "/images/technologies/python.svg",
+        color: "#0d9aff"
+    },
+    {
+        name: "Ubuntu",
+        icon: "/images/technologies/ubuntu.svg",
+        color: "#0d9aff"
+    },
+    {
+        name: "NodeJS",
+        icon: faNodeJs,
+        color: "#0d9aff"
+    },
+    {
+        name: "AWS",
+        icon: faAws,
+        color: "#0d9aff"
+    },
+    {
+        name: "Vaadin",
+        icon: faVaadin,
+        color: "#0d9aff"
+    },
+    {
+        name: "Sass",
+        icon: faSass,
+        color: "#0d9aff"
+    },
+    {
+        name: "PHP",
+        icon: faPhp,
+        color: "#0d9aff"
+    },
+    {
+        name: "GulpJS",
+        icon: faGulp,
+        color: "#0d9aff"
+    },
+    {
+        name: "MySQL",
+        icon: "/images/technologies/mysql.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "MongoDB",
+        icon: "/images/technologies/mongodb.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "Oracle Database",
+        icon: "/images/technologies/oracle.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "Microsoft SQL Server",
+        icon: "/images/technologies/microsoft-sql-server.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "Liquibase",
+        icon: "/images/technologies/liquibase.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "Kubernetes",
+        icon: "/images/technologies/kubernetes.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "Gradle",
+        icon: "/images/technologies/gradle.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "Material UI",
+        icon: "/images/technologies/material-ui.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "NextJS",
+        icon: "/images/technologies/nextjs.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "TypeScript",
+        icon: "/images/technologies/typescript.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "Jenkins",
+        icon: "/images/technologies/jenkins.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "Azure DevOps",
+        icon: "/images/technologies/azure-devops.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "JQuery",
+        icon: "/images/technologies/jquery.svg",
+        color: "#6db33f"
+    },
+    {
+        name: "Xampp",
+        icon: "/images/technologies/xampp.svg",
+        color: "#6db33f"
+    }
+]
---
+++ models/TechnologyModel.ts
@@ -0,0 +1,7 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+export interface TechnologyModel {
+    icon: IconProp | string;
+    name: string;
+    color: string;
+}
