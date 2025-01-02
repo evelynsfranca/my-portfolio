c447be39552f41b1d96235e250d91e7dcd7b2944
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-02 09:56:09 -0300

    chore: Footer - refactor - icons hover color

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,46 +1,27 @@
 "use client";
 
 import { contacts } from "@/data/contacts";
-import { faHeart, faSquareFull } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 
 export default function Footer() {
 
   const currentPage = usePathname();
-  const [hoverStyle, setHoverStyle] = useState<number>();
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
+  
   return (
     <footer className={styles.footer}>
 
-      <article 
-        className={styles.text} 
-        onMouseEnter={() => setHoverStyle(contacts.length)}
-        onMouseLeave={() => setHoverStyle(undefined)}
-      >
+      <article className={styles.text}>
 
         <span>Desenvolvido por Evelyn França</span>
 
-        {isClient && (
-          <FontAwesomeIcon
-            className={styles.icon}
-            icon={faHeart}
-            mask={faSquareFull}
-            style={{
-              background: hoverStyle == contacts.length
-                ? 'linear-gradient(45deg, var(--primary-color-variant), var(--secondary-color-variant))'
-                : 'white'
-            }}
-          />
-        )}
+        <FontAwesomeIcon
+          className={styles.icon}
+          icon={faHeart}
+        />
       </article>
 
       {currentPage != "/"
@@ -54,21 +35,11 @@ export default function Footer() {
                 href={it.link}
                 target="_blank"
                 className={styles.link}
-                onMouseEnter={() => setHoverStyle(i)}
-                onMouseLeave={() => setHoverStyle(undefined)}
               >
-                {isClient && (
-                  <FontAwesomeIcon
-                    className={styles.icon}
-                    icon={it.icon}
-                    mask={faSquareFull}
-                    style={{
-                      background: hoverStyle == i
-                        ? 'linear-gradient(45deg, var(--primary-color-variant), var(--secondary-color-variant))'
-                        : 'white'
-                    }}
-                  />
-                )}
+                <FontAwesomeIcon
+                  className={styles.icon}
+                  icon={it.icon}
+                />
               </Link>
             ))}
 
