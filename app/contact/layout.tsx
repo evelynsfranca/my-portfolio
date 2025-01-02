82ee01c0bb2cb99dedd9bb08f2a28027fee51e01
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-02 15:45:50 -0300

    fix: build fixes - Suspense

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Contato"
 };
 
 export default function ContactLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return <Suspense>{children}</Suspense>
 }
 
