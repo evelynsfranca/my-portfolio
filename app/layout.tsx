b4e1684006dc844d528878437378eb727902b25c
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-03 11:35:33 -0300

    chore: GoogleAnalytics tag - added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Menu from "@/components/Menu";
 import Footer from "@/components/Footer";
-import Script from 'next/script';
+import Menu from "@/components/Menu";
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Script from 'next/script';
+import "./globals.css";
 
 config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-br">      
       <Script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.x.x/css/all.min.css" />
+
+      <GoogleAnalytics gaId={"G-1ZN4MFRD46"} />
+
       <body className={inter.className}>
         <Menu />
         {children}
