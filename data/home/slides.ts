8c1a734c1f2ca66c11f6a1e1bb80d7c1a28b8b41
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-04 15:35:14 -0300

    chore: Images - optimization - JPG and JPEG to WEBP

--- data/home/slides.ts
+++ data/home/slides.ts
@@ -5,25 +5,25 @@ export const slides: SlideModel[] = [
     {
         title: services[0].title,
         subtitle: "Destaque sua presença online com um site profissional. ",
-        backgroundImage: "/images/slides/01/01.jpeg",
+        backgroundImage: "/images/slides/01.webp",
         link: "/servicos#" + services[0].id
     },
     {
         title: services[1].title,
         subtitle: "Seu aplicativo funcionando em Android e iOS com um só desenvolvimento. Comece agora!",
-        backgroundImage: "/images/slides/01/02.jpeg",
+        backgroundImage: "/images/slides/02.webp",
         link: "/servicos#" + services[1].id
     },
     {
         title: services[2].title,
         subtitle: "Melhore sua visibilidade online com otimizações avançadas de SEO.",
-        backgroundImage: "/images/slides/01/03.jpeg",
+        backgroundImage: "/images/slides/03.webp",
         link: "/servicos#" + services[2].id
     },
     {
         title: services[3].title,
         subtitle: "Converta mais visitantes em clientes com uma landing page estratégica.",
-        backgroundImage: "/images/slides/01/04.jpeg",
+        backgroundImage: "/images/slides/04.webp",
         link: "/servicos#" + services[3].id
     }
 ];
--- data/services/services.ts
+++ data/services/services.ts
@@ -7,7 +7,7 @@ export const services: ServiceModel[] = [
         title: "Desenvolvimento de sites",
         shortDescription: "Ofereço serviços de desenvolvimento de sites personalizados e responsivos, criados para atender às necessidades específicas de cada cliente. Com foco em design intuitivo e performance, os sites são otimizados para proporcionar uma experiência de navegação fluida em qualquer dispositivo. Utilizando as tecnologias mais recentes, desenvolvo sites que combinam funcionalidade e estética, ajudando a destacar a presença online do seu negócio.",
         icon: faComputer,
-        image: "/images/services/01/01.jpeg",
+        image: "/images/services/01.webp",
         description: `Desenvolvimento de sites personalizados e responsivos, criados para atender às necessidades específicas de cada cliente. Com foco em design intuitivo e performance, os sites são otimizados para proporcionar uma experiência de navegação fluida em qualquer dispositivo. Utilizando as tecnologias mais recentes, desenvolvo sites que combinam funcionalidade e estética, ajudando a destacar a presença online do seu negócio.
       `
     },
@@ -19,7 +19,7 @@ export const services: ServiceModel[] = [
             `Desenvolvimento de aplicativos híbridos para Android e iOS, com uma abordagem que une eficiência e custo-benefício. Com uma única base de código, consigo desenvolver apps que funcionam perfeitamente em ambas as plataformas, reduzindo o tempo de lançamento no mercado. Cada aplicativo é projetado com foco em usabilidade e desempenho, garantindo uma experiência de usuário excelente e compatível com os padrões de ambas as lojas de aplicativos.
         `,
         icon: faDesktop,
-        image: "/images/services/01/02.jpeg"
+        image: "/images/services/02.webp"
     },
     {
         id: "implementacao-de-seo",
@@ -29,7 +29,7 @@ export const services: ServiceModel[] = [
             `Realizo configurações e otimizações de SEO para melhorar o posicionamento do seu site nos motores de busca. Meu trabalho inclui análise de palavras-chave, otimização de conteúdo, melhorias na velocidade de carregamento e implementação de técnicas avançadas para melhorar a visibilidade do site. Com uma abordagem focada em resultados, ajudo a aumentar a relevância e a atração orgânica para seu site, garantindo que seu público-alvo o encontre com facilidade.
         `,
         icon: faTerminal,
-        image: "/images/services/01/03.jpeg"
+        image: "/images/services/03.webp"
     },
     {
         id: "desenvolvimento-de-landing-pages",
@@ -39,6 +39,6 @@ export const services: ServiceModel[] = [
             `Desenvolvimento de landing pages eficazes e visualmente atraentes, projetadas para converter visitantes em clientes. Cada landing page é construída com base em princípios de design e técnicas de SEO, com elementos que conduzem o visitante ao objetivo desejado, seja uma inscrição, venda ou contato. Utilizando práticas modernas de marketing digital, crio páginas com carregamento rápido e compatibilidade total em dispositivos móveis.
         `,
         icon: faChartSimple,
-        image: "/images/services/01/04.jpeg"
+        image: "/images/services/04.webp"
     }
 ];
