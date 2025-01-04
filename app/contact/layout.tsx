a11c40e51cd3726647ffd710446351ea95eca1aa
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-04 11:13:56 -0300

    chore: Contact - SEO Meta Tags

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "Contato",
-  description: "Solicite seu orçamento agora! Entre em contato para discutir projetos de desenvolvimento web, criação de aplicativos ou otimização de SEO. Retornamos rapidamente!",
+  title: "Contato | Desenvolvimento de Sites e Landing Pages",
+  description: "Solicite seu orçamento agora! Entre em contato para discutir projetos de desenvolvimento web, criação de aplicativos ou otimização de SEO. Retorno rapidamente!",
   keywords: ["Evelyn França", "Contato", "Solicitar Orçamento", "Redes Sociais", "Desenvolvimento de Sites", "Criação de Aplicativos", "Landing Pages", "SEO", "Integrações Personalizadas", "Serviços Web"]
 };
 
