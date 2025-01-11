b056af0e5b08518b299e5b2454cbbc7d2249a2e5
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-11 15:30:52 -0300

    chore: Projects - refactor

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,30 +1,42 @@
-import { projects } from "@/mocks/projects/projects";
+import { projects } from "@/data/projects/projects";
+import { Metadata } from "next";
 import ProjectCard from "./components/Card";
 import styles from "./page.module.css";
-import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Projetos"
 };
 
-
-export default function Projects() {
+export default function Projects() {  
   return (
     <main id="projects" className={styles.main}>
+
       <section className={styles.section}>
 
         <header className={styles.header}>
           <h1 className={styles.title}>Projetos</h1>
-          <h2 className={styles.subTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate odio, consectetur dolorum atque quidem aliquid temporibus placeat tempora modi debitis. Eligendi quibusdam necessitatibus culpa labore explicabo, iste laudantium esse quisquam.</h2>
+
+          <h2 className={styles.subTitle}>
+            Nesta seção, você encontrará uma coleção dos projetos que desenvolvi ao longo da minha trajetória como desenvolvedora de software. Cada projeto reflete desafios enfrentados, soluções criativas e o aprendizado adquirido ao aplicar diversas tecnologias e metodologias.
+          </h2>
+          <h2 className={styles.subTitle}>
+            Explore os detalhes de cada projeto e veja como utilizei ferramentas como Java, Spring Boot, ReactJS, MySQL, Azure, entre outras, para criar soluções funcionais e impactantes.
+          </h2>
+          <h2 className={styles.subTitle}>
+            Clique em qualquer card para saber mais sobre o projeto e seu processo de desenvolvimento ou visitá-lo!
+          </h2>
+
         </header>
 
         <article className={styles.content}>
+
           {projects.map(it => (
             <ProjectCard
               key={it.id}
               {...it}
             />
           ))}
+
         </article>
 
       </section>
