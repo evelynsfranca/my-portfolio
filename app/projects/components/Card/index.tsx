94e5adcd347c4e945e7c0a9f829a21da69866e9f
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-11 16:16:52 -0300

    chore: ProjectCard - component - refactor

--- app/projects/components/Card/index.tsx
+++ app/projects/components/Card/index.tsx
@@ -1,58 +1,99 @@
+"use client";
 
-import styles from "./index.module.css";
-import Image from "next/image";
-import defaultProjectImage from '@/public/images/default.svg';
-import { ProjectModel } from "@/models/ProjectModel";
 import ButtonLink from "@/components/Button/Link";
+import { projectFlags } from "@/data/projects/projectFlags";
+import { projectTypes } from "@/data/projects/projectTypes";
+import { ProjectModel } from "@/models/ProjectModel";
+import { useEffect, useState } from "react";
+import { fetchImages } from "../../api";
+import styles from "./index.module.css";
 
 export type ProjectCardProps = ProjectModel;
 
 export default function ProjectCard(props: Readonly<ProjectCardProps>) {
 
-    const { id, name, link, shortDescription, images } = props;
+    const { id, name, shortDescription, versions, flag } = props;
 
-    const projectImage = images != undefined && images.length > 0 ? images[0].url : defaultProjectImage;
+    const [showDetails, setShowDetails] = useState<boolean>(false);
+    const [image, setImage] = useState<string>("");
+
+    const type = versions[0].type;
+
+    useEffect(() => {
+        let documentSize = document.body.offsetWidth;
+
+        if (documentSize <= 640)
+            setShowDetails(true);
+
+
+        async function fetchImage() {
+            const data = await fetchImages(id, versions[0].tag);
+            setImage(data?.images ? data?.images[0]?.url : "");
+        }
+
+        fetchImage();
+    }, []);
 
     return (
-        <article className={styles.card}>
+        <article
+            className={`${styles.card}`}
+            style={{
+                backgroundImage: `url(${image})`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "auto 100%"
+            }}
+            onMouseEnter={() => setShowDetails(true)}
+            onMouseLeave={() => setShowDetails(false)}
+        >
+            <div style={{ opacity: showDetails ? .9 : 0 }}>
 
-            <header className={styles.cardHeader}>
-                <h3 className={styles.cardTitle}>{name}</h3>
-            </header>
+                <header className={styles.header}>
+                    <h3 className={styles.title}>{name}</h3>
+                </header>
 
-            <div className={styles.cardImage}>
-                <Image
-                    src={projectImage}
-                    alt={`Imagem do projeto ${name}`}
-                    width={0}
-                    height={0}
-                    sizes="100vw"
+                <div
+                    className={styles.description}
+                    dangerouslySetInnerHTML={{ __html: shortDescription }}
                 />
+
+                <div className={styles.tags}>
+                    <span
+                        className={styles.type}
+                        style={{ backgroundColor: projectTypes.filter(it => it.name == type)[0].color }}
+                    >
+                        {type}
+                    </span>
+
+                    <span
+                        className={styles.flag}
+                        style={{ backgroundColor: projectFlags.filter(it => it.name == flag)[0].color }}
+                    >
+                        {flag}
+                    </span>
+                </div>
+
+                <footer className={styles.footer}>
+
+                    <ButtonLink
+                        label="Detalhes"
+                        url={"/projetos/" + id}
+                        color="secondary"
+                        type="link"
+                        icon="none"
+                        target="_self"
+                    />
+                    <ButtonLink
+                        label="Visitar"
+                        url={versions.findLast(it => it)?.links?.app ?? "www.evelynfranca.com"}
+                        color="primary"
+                        type="link"
+                        icon="default"
+                    />
+                    
+                </footer>
             </div>
-
-            <div
-                className={styles.cardDescription}
-                dangerouslySetInnerHTML={{ __html: shortDescription }}
-            />
-
-            <footer className={styles.cardFooter}>
-                <ButtonLink
-                    label="Detalhes"
-                    url={"/projects/" + id}
-                    color="primary"
-                    type="button"
-                    icon="none"
-                    target="_self"
-                />
-                <ButtonLink
-                    label="Visitar"
-                    url={link}
-                    color="secondary"
-                    type="button"
-                    icon="default"
-                />
-            </footer>
-
         </article>
+
     );
 }
