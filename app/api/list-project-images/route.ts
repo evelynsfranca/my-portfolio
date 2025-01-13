effce773bd1e5f623253f6ac9737d6c743b3fd9d
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-13 11:06:05 -0300

    fix: api/list-project-images - removed mobile folder from result

--- app/api/list-project-images/route.ts
+++ app/api/list-project-images/route.ts
@@ -13,25 +13,29 @@ export async function GET(req: NextRequest): Promise<NextResponse<Data>> {
   const searchParams = req.nextUrl.searchParams;
   const projectId = searchParams.get("projectId");
   const projectVersion = searchParams.get("projectVersion") ?? "v1";
+  const mobile = searchParams.get("mobile");
 
   const defaultImageDir = "/images/default.svg";
 
   if (projectId && projectVersion) {
-    const dir = "/images/projects/" + projectId + "/" + projectVersion + "/";
+    const dir = "/images/projects/" + projectId + "/" + projectVersion + "/" + (mobile == "true" ? "mobile/" : "");
     const directoryPath = path.join(process.cwd(), '/public', dir);
+    console.log('dir: ', dir)
 
     try {
       const images = fs.readdirSync(directoryPath); // Lê os arquivos do diretório
 
-      let response = images.map(it => {
-        let url = dir + it;
-        let alt = it.split('.')[0].replace(/[0-9]/g, '').replaceAll('_', ' ').replace('-', '').replace('-', ' - ');
+      let response = images
+        .filter(it => it != "mobile")
+        .map(it => {
+          let url = dir + it;
+          let alt = it.split('.')[0].replace(/[0-9]/g, '').replaceAll('_', ' ').replace('-', '').replace('-', ' - ');
 
-        return {
-          url,
-          alt
-        }
-      })
+          return {
+            url,
+            alt
+          };
+        })
 
       return NextResponse.json({ images: response });
 
