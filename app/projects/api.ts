d815a7c9c36e3480330d1ffc5bd05b515bee2a0b
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-13 11:26:06 -0300

    fix: projects/api - added mobile option

--- app/projects/api.ts
+++ app/projects/api.ts
@@ -1,7 +1,7 @@
 import { Data } from "../api/list-project-images/route";
 import { API_URL } from "../shared/utils/variables";
 
-export async function fetchImages(projectId: string, projectVersion: string): Promise<Data> {
-    return await fetch(API_URL + '/list-project-images?projectId=' + projectId + '&projectVersion=' + projectVersion)
+export async function fetchImages(projectId: string, projectVersion: string, mobile?: boolean): Promise<Data> {
+    return await fetch(API_URL + '/list-project-images?projectId=' + projectId + '&projectVersion=' + projectVersion + '&mobile=' + mobile)
         .then(res => res.json());
 }
