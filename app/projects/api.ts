ed56e91da26c429c8df4a7b6385ca28c3025382d
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-15 20:27:35 -0300

    chore: projects/api - get-details - added

--- app/projects/api.ts
+++ app/projects/api.ts
@@ -1,7 +1,13 @@
-import { Data } from "../api/list-project-images/route";
+import { ProjectDetails } from "../api/projects/get-details/route";
+import { Data } from "../api/projects/list-images/route";
 import { API_URL } from "../shared/utils/variables";
 
 export async function fetchImages(projectId: string, projectVersion: string, mobile?: boolean): Promise<Data> {
-    return await fetch(API_URL + '/list-project-images?projectId=' + projectId + '&projectVersion=' + projectVersion + '&mobile=' + mobile)
+    return await fetch(API_URL + '/projects/list-images?projectId=' + projectId + '&projectVersion=' + projectVersion + '&mobile=' + mobile)
+        .then(res => res.json());
+}
+
+export async function fetchDetails(projectId: string, projectVersion: string): Promise<ProjectDetails> {
+    return await fetch(API_URL + '/projects/get-details?projectId=' + projectId + '&projectVersion=' + projectVersion)
         .then(res => res.json());
 }
