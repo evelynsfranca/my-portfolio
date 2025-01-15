80bc664191dac8f1a6edf5ef45dd94ac66a9624a
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-15 20:26:22 -0300

    chore: api/projects/list-images - refactor

--- app/api/projects/list-images/route.ts
+++ app/api/projects/list-images/route.ts
@@ -11,26 +11,30 @@ export async function GET(request: Request): Promise<NextResponse<Data>> {
   const { searchParams } = new URL(request.url);
 
   const projectId = searchParams.get("projectId");
-  const projectVersion = searchParams.get("projectVersion") ?? "v1";
+  const projectVersion = searchParams.get("projectVersion") ?? "1.0";
   const mobile = searchParams.get("mobile");
 
   const dir =
-    projectId && projectVersion
+    projectId
       ? "projects/images/" + projectId + "/" + projectVersion + "/" + (mobile == "true" ? "mobile/" : "")
       : "/projects/images/default.svg"
 
   try {
     // ⚠️ The below code is for App Router Route Handlers only
-    let response = await list({ prefix: dir })
+    let response = await list({ prefix: dir, mode: 'folded' })
       .then(res => {
+
         return res.blobs.length > 0
           && res.blobs
-            .filter((it) => it.pathname.split('/')[4] != "mobile")
             .filter((it) => it.size != 0)
             .map(it => {
               let url = it.url;
-              let alt = it.pathname
-                .split('/')[4]
+              let list = 
+                mobile == undefined || mobile == 'false'
+                  ? it.pathname.split('/')[4] 
+                  : it.pathname.split('/')[5];
+
+              let alt = list
                 .split('.')[0]
                 .replace(/[0-9]/g, '')
                 .replaceAll('_', ' ')
