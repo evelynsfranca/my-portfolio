c1766cb23f066e7249e23cd0618711170eb83236
Evelyn França <evelynsfrancaa@gmail.com>
2025-01-15 20:26:46 -0300

    chore: api/projects/get-details - added

---
+++ app/api/projects/get-details/route.ts
@@ -0,0 +1,45 @@
+import { list } from '@vercel/blob';
+import { NextResponse } from 'next/server';
+
+export type ProjectDetails = {
+  content: string;
+  error?: string;
+};
+
+export async function GET(request: Request): Promise<NextResponse<ProjectDetails>> {
+  const { searchParams } = new URL(request.url);
+
+  const projectId = searchParams.get("projectId");
+  const projectVersion = searchParams.get("projectVersion") ?? "1.0";
+
+  const dir =
+    projectId && projectVersion
+      ? "projects/details/" + projectId + "/" + projectVersion + "/description.html"
+      : "projects/details/default.html";
+
+      const defaultURL = process.env.BLOB_HOSTNAME + "/projects/details/default.html"
+
+  try {
+    let response = await list({ prefix: dir, mode: 'folded' })
+      .then(res => {
+        return res.blobs.length > 0
+          && res.blobs
+            .filter((it) => it.size != 0)
+            .map(it => it.url)[0];
+      });
+
+    if (response) {
+      return NextResponse.json({ content: response ?? defaultURL });
+    } else {
+      return NextResponse.json({
+        content: defaultURL
+      });
+    }
+
+  } catch (error) {
+    console.error('Error reading directory:', error);
+    return NextResponse.json({
+      content: defaultURL
+    });
+  }
+}
